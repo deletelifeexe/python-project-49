c70fca0c4fe24d331dede2e8a5080534002e8950
deletelifeexe <deletelifeexe@proton.me>
2026-09-22 15:52:00 +1000

    refactor: launch logic retained; functionality moved to engine.py.

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,56 +1,10 @@
-import operator
-import random
-
-import prompt
-
-
-def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
-
-
-OPS = {"*": operator.mul,
-       "+": operator.add,
-       "-": operator.sub}
-
-
-def brain_calc(name):
-    count_win_answers = 0
-
-    print('What is the result of the expression?')
-
-    while count_win_answers != 3:
-
-        random_numb_first = random.randint(1, 100)
-        random_numb_second = random.randint(1, 100)
-        random_sign = random.choice(list(OPS.keys()))
-
-        expression = f'{random_numb_first} {random_sign} {random_numb_second}'
-        print(f"Question: {expression}")
-        result = OPS[random_sign](random_numb_first, random_numb_second)
-
-        answer = prompt.integer('Your answer: ')
-        if answer == result:
-            count_win_answers += 1
-            print('Correct!')
-
-        else:
-            print(f"'{answer}' is wrong answer ;(. "
-                  f"Correct answer was '{result}'.")
-            print(f"Let's try again, {name}!")
-            return
-    print(f"Congratulations, {name}!")
+from brain_games import engine
+from brain_games.games import calc
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = welcome_user()
-    brain_calc(name)
+    engine.run_game(calc)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,46 +1,10 @@
-import random
-
-import prompt
-
-
-def welcome_user():
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
-
-
-def parity_check(number):
-    return 'yes' if number % 2 == 0 else 'no'
-
-
-def brain_even(name):
-    count_win_answers = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    while count_win_answers != 3:
-        random_number = random.randint(1, 100)
-        print(f'Question: {random_number}')
-        correct = parity_check(random_number)
-        answer = prompt.string('Your answer: ')
-        if answer == correct:
-            count_win_answers += 1
-            print('Correct!')
-
-        else:
-            print(f"{answer} is wrong answer ;(. "
-                  f"Correct answer is was {parity_check(random_number)}")
-            print(f"Let's try again, {name}!")
-            return
-    print(f"Congratulations, {name}!")
+from brain_games import engine
+from brain_games.games import even
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = welcome_user()
-    brain_even(name)
+    engine.run_game(even)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
-
-
